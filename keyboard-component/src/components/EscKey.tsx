209db92776cc2d9873d2e3e61e066a2dff5b4d83
Aman <aman.ver9345@gmail.com>
2026-01-19 19:22:07 +0530

    added another key component

--- keyboard-component/src/components/EscKey.tsx
+++ keyboard-component/src/components/EscKey.tsx
@@ -1,8 +1,8 @@
 const EscKey = () => {
   return (
-    <div className="flex items-end w-15 h-8.75 rounded-md bg-black ring ring-neutral-700 shadow-[0_4px_4px_0.5px_rgba(255,255,255,0.5)] hover:w-14.5 hover:h-8 hover:shadow-none transition-all duration-100">
-      <div className="ml-2 mb-0.5">
-        <span className="text-white text-[7px]">esc</span>
+    <div className="flex items-end w-15 h-8 rounded-md bg-black ring ring-neutral-700 shadow-[0_3px_4px_0.2px_rgba(255,255,255,0.5)] hover:w-14.5 hover:h-7.5 hover:shadow-none transition-all duration-100">
+      <div className="ml-1.5 mb-1">
+        <div className="text-white text-[7px]">esc</div>
       </div>
     </div>
   );
--- keyboard-component/src/pages/Keyboard.tsx
+++ keyboard-component/src/pages/Keyboard.tsx
@@ -1,10 +1,31 @@
-import EscKey from "../components/EscKey"
+import EscKey from "../components/EscKey";
+import NormalKey from "../components/NormalKey";
+
 
 
 const Keyboard = () => {
   return (
-    <div className="bg-neutral-800 w-2xl h-70 rounded-xl p-2">
+    <div className="grid grid-cols-1 grid-rows-6 items-start bg-neutral-900 w-141.5 h-60 rounded-xl p-1.5">
+      <div className="flex gap-1.5 row-span-1">
       <EscKey />
+      <NormalKey keyName="F1" />
+      <NormalKey />
+      <NormalKey />
+      <NormalKey />
+      <NormalKey />
+      <NormalKey />
+      <NormalKey />
+      <NormalKey />
+      <NormalKey />
+      <NormalKey />
+      <NormalKey />
+      <NormalKey />
+      <NormalKey />
+      </div >
+      <div className="flex gap-1.5 row-span-1">
+      <NormalKey />
+
+      </div>
       
     </div>
   )
