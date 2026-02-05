32a4a55877164429b7ddbb4dd6256de15286d5f0
Aman <aman.ver9345@gmail.com>
2026-02-05 22:51:15 +0530

    added new files

--- responsive-design/src/components/Responsive.tsx
+++ responsive-design/src/components/Responsive.tsx
@@ -2,8 +2,10 @@
 
 const Responsive = () => {
   return (
-    <div className="text-4xl font-bold lg:bg-blue-500">
-      Responsive
+    <div className="flex flex-col md:flex-row items-center gap-10">
+      <div className="h-40 w-40 bg-neutral-200 rounded-2xl"></div>
+      <div className="h-40 w-40 bg-red-200 rounded-2xl"></div>
+      <div className="h-40 w-40 bg-blue-200 rounded-2xl"></div>
     </div>
   )
 }
